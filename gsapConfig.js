5a5a83cac30ee18881d486e2974b07998d5534d8
Deadend1415 <ahipuedaniels@gmail.com>
2024-10-30 00:50:16 +0100

    project initialised

---
+++ gsapConfig.js
@@ -0,0 +1,6 @@
+ // use a script tag or an external JS file
+    document.addEventListener("DOMContentLoaded", (event) => {
+    // gsap code here!
+        gsap.to("div", { rotation: 180, x: 200, duration: 2, yoyo: true, repeat:3 });
+});
+
