0cddd31cfa8208a39f3da8b1c6384d95ab7c8087
Deadend1415 <ahipuedaniels@gmail.com>
2024-10-31 15:23:10 +0100

    Project Completed
    -added minor styling
    -added final animations

--- gsapConfig.js
+++ gsapConfig.js
@@ -1,27 +1,29 @@
- // use a script tag or an external JS file
+ // global functions
+ function relativeValues(value) {
+     vw = innerWidth;
+     return value/100*vw;
+ }
+
  document.addEventListener("DOMContentLoaded", (event) => {
     // gsap code here!
-        function relativeValues(value) {
-            vw = innerWidth;
-            return value/100*vw;
-        }
         gsap.registerPlugin(TextPlugin,Flip);
         // Declaring variables and creates a timeline
         let tl = gsap.timeline()
         let tl2 = gsap.timeline()
-        div1 = $(".movingDiv");
+        div1 = $("h1");
         //configure timelines
         tl.from(".lilDiv", { rotation: -360, x: relativeValues(-50), duration: 2, delay:1, stagger: .5,opacity: 0,ease: "expoScale(0.5,7,power2.in)",})
             .from("h1", {y: -200, ease: "bounce", opacity: 0, duration: 2, stagger: 1}, "-=1")
-            .to("h1:nth-child(2)", {text: {value: " crazy!!!", speed: 3, newClass: "text-warning"}});
+            .to("h1:nth-child(2)", {text: {value: " Orihime!", speed: 3, newClass: "text-warning"}});
 
-        tl2.to(".movingDiv",{height: 400, ease:"bounce"});
 
         div1.mouseenter(function() {
+            tl2.to(".movingDiv",{height: 400, duration: 1, stagger: 1});
             tl2.play();
-        });
-        div1.mouseleave(function() {
-            tl2.reverse();
+            //play only if mouseout after hovers into the element
+            div1.mouseleave(function() {
+                tl2.reverse();
+            });
         });
 
 });
